b14614c1c01084838d5f3fdb4553b640c1ee2a02
imAnkitMeena <ankitmeena11102003@gmail.com>
2024-07-30 23:07:25 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,279 @@
+import express from "express";
+import { connectDB } from "./utils/features.js";
+import dotenv from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { v4 as uuid } from "uuid";
+import cors from "cors";
+import { v2 as cloudinary } from "cloudinary";
+import {
+  CHAT_JOINED,
+  CHAT_LEAVED,
+  NEW_MESSAGE,
+  NEW_MESSAGE_ALERT,
+  ONLINE_USERS,
+  START_TYPING,
+  STOP_TYPING,
+} from "./constants/events.js";
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/message.js";
+import { corsOptions } from "./constants/config.js";
+import { socketAuthenticator } from "./middlewares/auth.js";
+
+import userRoute from "./routes/user.js";
+import chatRoute from "./routes/chat.js";
+import adminRoute from "./routes/admin.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 3000;
+const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+const adminSecretKey = process.env.ADMIN_SECRET_KEY || "adsasdsdfsdfsdfd";
+const userSocketIDs = new Map();
+const onlineUsers = new Set();
+
+connectDB(mongoURI);
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: corsOptions,
+});
+
+app.set("io", io);
+
+// Using Middlewares Here
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/chat", chatRoute);
+app.use("/api/v1/admin", adminRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+io.use((socket, next) => {
+  cookieParser()(
+    socket.request,
+    socket.request.res,
+    async (err) => await socketAuthenticator(err, socket, next)
+  );
+});
+
+io.on("connection", (socket) => {
+  const user = socket.user;
+  userSocketIDs.set(user._id.toString(), socket.id);
+
+  socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+    const messageForRealTime = {
+      content: message,
+      _id: uuid(),
+      sender: {
+        _id: user._id,
+        name: user.name,
+      },
+      chat: chatId,
+      createdAt: new Date().toISOString(),
+    };
+
+    const messageForDB = {
+      content: message,
+      sender: user._id,
+      chat: chatId,
+    };
+
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(NEW_MESSAGE, {
+      chatId,
+      message: messageForRealTime,
+    });
+    io.to(membersSocket).emit(NEW_MESSAGE_ALERT, { chatId });
+
+    try {
+      await Message.create(messageForDB);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
+  socket.on(START_TYPING, ({ members, chatId }) => {
+    const membersSockets = getSockets(members);
+    socket.to(membersSockets).emit(START_TYPING, { chatId });
+  });
+
+  socket.on(STOP_TYPING, ({ members, chatId }) => {
+    const membersSockets = getSockets(members);
+    socket.to(membersSockets).emit(STOP_TYPING, { chatId });
+  });
+
+  socket.on(CHAT_JOINED, ({ userId, members }) => {
+    onlineUsers.add(userId.toString());
+
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(ONLINE_USERS, Array.from(onlineUsers));
+  });
+
+  socket.on(CHAT_LEAVED, ({ userId, members }) => {
+    onlineUsers.delete(userId.toString());
+
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(ONLINE_USERS, Array.from(onlineUsers));
+  });
+
+  socket.on("disconnect", () => {
+    userSocketIDs.delete(user._id.toString());
+    onlineUsers.delete(user._id.toString());
+    socket.broadcast.emit(ONLINE_USERS, Array.from(onlineUsers));
+  });
+});
+
+app.use(errorMiddleware);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port} in ${envMode} Mode`);
+});
+
+export { envMode, adminSecretKey, userSocketIDs };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// import userRoute from "./routes/user.js";
+// import chatRoute from "./routes/chat.js";
+// import adminRoute from "./routes/admin.js";
+// import { connectDB } from "./utils/features.js";
+// import dotenv from "dotenv";
+
+// import cookieParser from "cookie-parser";
+// import { errorMiddleware } from "./middlwares/error.js";
+// import { Server} from "socket.io";
+// import { createServer } from "http";
+// import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from "./constants/events.js";
+// import { v4 as uuid } from "uuid";
+// //import { name } from "@cloudinary/url-gen/actions/namedTransformation";
+// import { getSockets } from "./lib/helper.js";
+// import { Message } from "./models/message.js";
+
+// dotenv.config({
+//     path: "./.env",    
+// });
+
+  
+// const mongoURI= process.env.MONGO_URI;
+// const port = process.env.PORT || 3000;
+// const envMode = process.env.NODE_ENV.trim() || "PRODUCTION"; 
+// const adminSecretKey = process.env.ADMIN_SECRET_KEY || "adsasdsdfsdfsdfd";
+// const userSocketIDs = new Map();
+
+// connectDB(mongoURI);  
+
+// const app = express();
+
+// const server = createServer(app);
+//  const io = new Server(server, {
+// // //   cors: corsOptions,
+// });
+
+// app.use(express.json()); 
+// app.use(cookieParser());  
+ 
+// app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/chat", chatRoute);
+// app.use("/api/v1/admin",adminRoute);
+
+ 
+// app.get("/", (req, res) => {
+//     res.send("Hallo ");
+// });
+ 
+// io.use((socket,next)=>{});//frontend
+ 
+//   io.on("connection",(socket)=>{
+//     const user={
+//       _id:"asdsda",
+//       name:"Namgon",
+//     }
+//     // const user = socket.user;
+//    userSocketIDs.set(user._id.toString(), socket.id);
+
+//     console.log(userSocketIDs);
+   
+   
+//     socket.on(NEW_MESSAGE, async(chatId, members,message)=>{
+     
+//       const messageForRealTime={
+//         content:message,
+//         _id:uuid(),
+//         sender:{
+//           _id:user._id,
+//           name:user.name, 
+//         },      
+//    chat:chatId,
+//    createdAt: new Date().toISOString(),
+//       }; 
+
+//     const messageForDB={
+//       content:message,
+//       sender:user._id,
+//       chat:chatId,
+        
+//     };
+//     const membersSocket =getSockets(members);
+//      io.to(membersSocket).emit(NEW_MESSAGE,{
+//       chatId,
+//       message:messageForRealTime,
+//      });
+//      io.to(membersSocket).emit(NEW_MESSAGE_ALERT,{chatId});
+     
+//     try {
+//       await Message.create(messageForDB);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     });
+
+
+           
+//     socket.on("disconnected", ()=>{
+
+//       console.log("User Disconnected");
+//       userSocketIDs.delete(user._id.toString());
+//     });
+//   });
+
+//   app.use(errorMiddleware);
+  
+//   server.listen(port, () => { 
+//     console.log(`Server is running on port ${port} in ${envMode} Mode`);
+//   });
+  
+//   export { envMode, adminSecretKey, userSocketIDs }; 
---
+++ server/middlewares/multer.js
@@ -0,0 +1,42 @@
+import multer from "multer";
+
+const multerUpload = multer({
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
+
+const singleAvatar = multerUpload.single("avatar");
+
+const attachmentsMulter = multerUpload.array("files", 5);
+
+export { singleAvatar, attachmentsMulter };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import multer from "multer";
+
+// const multerUpload = multer({
+//   limits: {
+//     fileSize: 1024 * 1024 * 5,
+//   },
+// });
+
+// const singleAvatar = multerUpload.single("avatar");
+
+// const attachmentsMulter = multerUpload.array("files", 5);
+
+// export { singleAvatar, attachmentsMulter };
